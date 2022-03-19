05824459450a67cfba5295de6fb60f404a3e6c04
kasmediautamu <ivan.khabuyhe1@gmail.com>
2022-03-19 17:35:27 +0300

    adds routes structure

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,14 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {BrowserRouter} from "react-router-dom";
+import {Provider} from "react-redux";
+import {PersistGate} from "redux-persist/integration/react";
+
+import {store, persistor} from "./store/store";
 import "./index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+ <React.StrictMode>
+  <Provider store={store}>
+   <BrowserRouter>
+    <PersistGate persistor={persistor}>
+     <App />
+    </PersistGate>
+   </BrowserRouter>
+  </Provider>
+ </React.StrictMode>,
+ document.getElementById("root")
 );
 
 reportWebVitals();
